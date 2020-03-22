a7d73ac07047d4d2665bcba4f432ceca772eb0a1
Gabriel Aguinaga <gabrielaguinaga@Gabriels-MBP.lan1>
2020-03-22 05:41:41 -0500

    added withRouter, now routes to Hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+import Hats from './pages/hats/hats.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={Hats} />
     </div>
   );
 }
---
+++ src/pages/hats/hats.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './hats.styles.scss';
+const Hats = () => <div>Hats Page</div>;
+
+export default Hats;
