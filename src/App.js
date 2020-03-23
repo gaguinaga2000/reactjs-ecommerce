c5c3578352022cce2c27b642f9304f26b77eacbc
“Gabriel <gabriel.aguinaga@gmail.com>
2020-03-23 15:58:48 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import Hats from './pages/hats/hats.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
